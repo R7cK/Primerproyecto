2c1eb73a9a4cfc9b100a97067ff9053002aad101
Ricardo_Chab <chabricardo6@gmail.com>
2026-02-09 23:37:44 -0600

    actualizacion de vistas

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\PrincipalController;
 
 
-Route::get('/hello',HomeController::class);
+//Route::get('/hello',HomeController::class);
 Route::get('post/mensaje',[PostController::class,'Mensaje']);
 Route::get('post/about/param}/{name}',[PostController::class,'About']);
 /*Route::get('/', function () 
@@ -15,3 +15,11 @@ Route::get('post/about/param}/{name}',[PostController::class,'About']);
 
 Route::get('/empresa',[HomeController::class,'empresa'])->name('empresa');
 
+Route::get('/contact',function(){
+    $nombre="Ricardo Antonio Chab";
+    return view('contact',['nombre'=>$nombre, 'carrera'=>'Estudiante']);
+})->name('contact');
+
+Route::get('/',function(){
+    return view('welcome');
+})->name('vista_inicio');
